827e3ebc1133e095284770fd6f54d474f7576b22
Ahr1man <fa.vesn@gmail.com>
2016-06-02 15:09:07 +0500

    create

---
+++ OOP/Lab1/Lab1/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    abstract class Print
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public Print(string Name, string Author)
+        {
+            this.Name = Name;
+            this.Author = Author;
+        }
+
+        public abstract void Display();
+    }
+
+    public class Book : Print
+    {
+        public string Publishing_House { get; set; }
+        public string Year { get; set; }
+
+    }
+
+
+}
